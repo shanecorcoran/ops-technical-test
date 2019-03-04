7b4f1cdf9ae7187a07adaa06d548b8829a4a30db
kunalnanda <kunal.nanda@innovatoryapps.com>
2019-03-05 00:03:32 +1100

    Check data

--- app_test.py
+++ app_test.py
@@ -17,7 +17,7 @@ class TestFlaskApi(unittest.TestCase):
         response = self.app.get(BASE_URL)
         data = json.loads(response.get_data())
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(data['myapplication']), 1)
+        return data
 
     def test_get_healthcheck(self):
         response = self.app.get(HEALTHCHECK_URL)
