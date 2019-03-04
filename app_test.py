f974f9d2171bb5a2a545a51517378fefa2697036
kunalnanda <kunal.nanda@innovatoryapps.com>
2019-03-05 00:11:51 +1100

    Added root url check

--- app_test.py
+++ app_test.py
@@ -1,10 +1,12 @@
 from copy import deepcopy
 import unittest
 import json
+import urllib2
 
 import app
 
-BASE_URL = 'http://127.0.0.1:5000/todo/api/v1.0/myapplication'
+BASE_URL = 'http://127.0.0.1:5000/'
+METADATA_URL = 'http://127.0.0.1:5000/todo/api/v1.0/myapplication'
 HEALTHCHECK_URL = 'http://127.0.0.1:5000/healthcheck'
 
 class TestFlaskApi(unittest.TestCase):
@@ -14,16 +16,16 @@ class TestFlaskApi(unittest.TestCase):
         self.app.testing = True
 
     def test_get_myapplication(self):
-        response = self.app.get(BASE_URL)
-        data = json.loads(response.get_data())
+        response = self.app.get(METADATA_URL)
         self.assertEqual(response.status_code, 200)
-        return data
 
     def test_get_healthcheck(self):
         response = self.app.get(HEALTHCHECK_URL)
-        data = json.loads(response.get_data())
         self.assertEqual(response.status_code, 200)
-        #self.assertEqual(len(data['myapplication']), 1)
+
+    def test_get_root(self):
+        response = urllib2.urlopen(BASE_URL)
+        self.assertEqual(response.code, 200)
 
     def tearDown(self):
         # reset app.items to initial state
